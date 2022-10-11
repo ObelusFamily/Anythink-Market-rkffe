00d73a0300265e73cfbeeb6f1194e03787fbd469
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-10-11 23:23:45 +0200

    Added image placeholder+ Made conditional

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -49,12 +49,21 @@ class Item extends React.Component {
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
-              <img
-                src={this.props.item.image}
-                alt={this.props.item.title}
-                className="item-img"
-                style={{ height: "500px", width: "100%", borderRadius: "6px" }}
-              />
+              {this.props.item.image.length === 0 ? (
+                  <img
+                  src="/placeholder.png"
+                  alt="Placeholder Image"
+                  className="item-img"
+                  style={{ height: "500px", width: "100%", borderRadius: "6px" }}
+                  />
+                ) : (
+                  <img
+                  src={this.props.item.image}
+                  alt={this.props.item.title}
+                  className="item-img"
+                  style={{ height: "500px", width: "100%", borderRadius: "6px" }}
+                  />
+                )}
             </div>
 
             <div className="col-6">
